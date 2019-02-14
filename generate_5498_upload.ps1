206a1f32d502e1d2774de7a10f6cae91ac94079e
Dalton Curtin <dcurtin@midlandira.com>
2019-02-13 20:44:33 -0500

    Starting to develop params and use of a config file.

--- generate_5498_upload.ps1
+++ generate_5498_upload.ps1
@@ -1,10 +1,40 @@
-﻿#parameters
-$file= 'C:\Users\dcurtin\Desktop\5498_doc.csv'
-$AvailableInPortal='FALSE'
-$description='This substitute 5498 is not final and may be subject to change'
-$todays_date = Get-Date -UFormat '%m/%d/%Y'
-$doc_name = (Get-Date -UFormat '%Y-%m-%d') + ' Tax Document'
-$doc_type = 'Tax Document'
+﻿
+#parameters
+#config file
+#load values from that
+#get config path from command
+
+param (
+    [string]$conf_file='.\settings.conf',
+    [string]$file='C:\Users\dcurtin\Desktop\5498_doc.csv',
+    [string]$AvailableInPortal='FALSE',
+    [string]$description='This substitute 5498 is not final and may be subject to change',
+    [string]$todays_date = (Get-Date -UFormat '%m/%d/%Y'),
+    [string]$doc_name = (Get-Date -UFormat '%Y-%m-%d') + ' Tax Document',
+    [string]$doc_type = 'Tax Document'
+    #[string]$h='',
+    #[string]$help=''
+)
+
+if($args[0] -eq '-h')
+{
+    Write-Host 'in help'
+}
+
+if(test-path $conf_file)
+{
+    Get-Content .\thing_file | %{
+        $kv_pair=($_-split'=')
+        if(kv_pair[0] -eq file)
+        {
+            $file=kv_pair[1]
+        }
+        
+    }
+}
+
+return 0
+
 
 #
 echo '' > $file
@@ -35,6 +65,7 @@ Get-ChildItem '*.pdf' | % {
 
     if($_.FullName -match '\d{7}')
     {
+        Write-Host $_.BaseName
         $id = $HashTable[$matches[0]]
         $csv_file_array += New-Object PSObject -Property @{'ACCOUNT ID'=$id; 'ACCOUNT NAME'=$matches[0]; 'DESCRIPTION'=$description; 'Content Type'='application/pdf'; 'DOC TYPE'=$doc_type; 'DOC DATE'=$todays_date; 'DOC NAME'=$doc_name;' AVAILABLE IN PORTAL'=$AvailableInPortal; 'BODY'= $_}
     }
